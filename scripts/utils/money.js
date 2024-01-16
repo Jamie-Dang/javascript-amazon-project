dc19e010a12fa083cb0b84929383236fb7c794bf
JaimieDang <jaimiedang02@gmail.com>
2024-01-16 14:20:00 +0700

    Save data and generate HTML for payment summary

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,6 +1,6 @@
 // this file is going to contain utilities that are related to money
 export function formatCurrency(priceCents) {
-  return (priceCents / 100).toFixed(2);
+  return (Math.round(priceCents) / 100).toFixed(2);
 }
 
 export default formatCurrency;
